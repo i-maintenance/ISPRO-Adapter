061c0c7d6eed9a9f9e6ee6fa854747345e00a218
dglachs <dglachs@gmail.com>
2017-09-15 14:03:00 +0200

    runner according to kafka consumer tutorial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import ispro.ISPROAdapter;
 import ispro.impl.ISPROAdapterImpl;
 import kafka.ISPROConsumer;
 /**
@@ -10,16 +15,34 @@ import kafka.ISPROConsumer;
  */
 public class Main {
 	public static void main(String[] args) {
-		String groupId = "ipn-consumer-group";
-		// 
+		int numConsumers = 3;
+		String groupId = "imaintenance-ispro-group";
 		List<String> topics = Arrays.asList("node-red-message");
-		// create and run a new consumer
-		ISPROConsumer consumer = new ISPROConsumer(1, "il061:9092", groupId, new ISPROAdapterImpl(), topics);
-		Thread runner = new Thread(consumer);
-		
-		runner.start();
-		
-		
+		ExecutorService executor = Executors.newFixedThreadPool(numConsumers);
+
+		final List<ISPROConsumer> consumers = new ArrayList<>();
+		final ISPROAdapter isproAdapter = new ISPROAdapterImpl();
+		for (int i = 0; i < numConsumers; i++) {
+			// 
+			ISPROConsumer consumer = new ISPROConsumer(i, "il061:9092", groupId, isproAdapter, topics);
+			consumers.add(consumer);
+			executor.submit(consumer);
+		}
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				for (ISPROConsumer consumer : consumers) {
+					consumer.shutdown();
+				}
+				executor.shutdown();
+				try {
+					executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
