61efb53ee8db6154a291cc75baffb98c727023ea
dglachs <dglachs@gmail.com>
2017-11-20 14:22:08 +0100

    structure for IPN QualityMessages

--- src/main/java/ipn/QualityMessage.java
+++ src/main/java/ipn/QualityMessage.java
@@ -3,13 +3,15 @@ package ipn;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-@JsonInclude(value=Include.NON_NULL)
+@JsonInclude(value = Include.NON_NULL)
 public class QualityMessage extends Message<ProductQuality> {
-	public QualityMessage() {
-		
-	}
-	public QualityMessage(ProductQuality q) {
-		super(q);
-	}
+    private ProductQuality result;
+    public QualityMessage() {
+
+    }
+
+    public QualityMessage(ProductQuality q) {
+	super(q);
+    }
 
 }
