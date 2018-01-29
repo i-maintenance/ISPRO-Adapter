e6bf97c5e288671cbf03f6dbae437eb176d147ae
dglachs <dglachs@gmail.com>
2018-01-29 15:55:02 +0100

    rename Main to ISPROAdapter

--- src/main/java/ispro/ISPROAdapter.java
+++ src/main/java/ispro/ISPROAdapter.java
@@ -1,11 +1,29 @@
 package ispro;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
+import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
+import eu.imaintenance.toolset.ToolsetClient;
+import ispro.impl.ISPROHandler;
 /**
- * Adapter processing the consumed messages
+ * Dummy runner
  * @author dglachs
  *
  */
-public interface ISPROAdapter {
-	boolean processMessage(String topic, String key, String payload);
-
+public class ISPROAdapter {
+	public static void main(String[] args) {
+	    try {
+            new ToolsetClient()
+                    .withServiceUri("http://il060:8082/v1.0/")
+                    .setName("ISPROClient")
+                    .registerHandler(new ISPROHandler(), 30l)
+                    .startup();
+        } catch (MalformedURLException | URISyntaxException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (ServiceFailureException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	}
 }
